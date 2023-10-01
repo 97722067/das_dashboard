0b6970c496ee28c6aefa03790c7378ee87f4288f
Lokesh Singh <ppp@ppps-MacBook-Pro.local>
2023-10-01 15:49:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import FileClaim from "./pages/fileClaim/FileClaim";
+
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes
+} from "react-router-dom";
+import ProcessClaim from "./pages/processclaim/ProcessClaim";
+
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home/>}/>
+            <Route path="login" element={<Login/>}/>
+            <Route path="fileclaim" element={<FileClaim/>}/>
+            <Route path="processclaim" element={<ProcessClaim/>}/>
+          </Route>
+        </Routes>
+     </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
